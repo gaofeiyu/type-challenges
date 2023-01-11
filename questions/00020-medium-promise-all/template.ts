0014ac7363a18f283e1e39ba8728d0935724dd41
gaofeiyu <feiyu.gao@gmail.com>
2023-01-11 21:03:11 +0800

    update

--- questions/00020-medium-promise-all/template.ts
+++ questions/00020-medium-promise-all/template.ts
@@ -5,3 +5,9 @@ declare function PromiseAll<T extends unknown[]>(
 ): Promise<{
   [K in keyof T]: PromiseFlat<T[K]>
 }>
+
+/**
+ * 这里主要是看返回值的灵活处理
+ * 简单来讲就是PromiseAll泛型中的类型和PromiseAll方法返回的Promise泛型中的类型要一致
+ * 但如果values对应的值是Promise类型，则要取到最终Promise泛型里面的类型
+ */
--- questions/00020-medium-promise-all/test-cases.ts
+++ questions/00020-medium-promise-all/test-cases.ts
@@ -2,12 +2,12 @@ import type { Equal, Expect } from '@type-challenges/utils'
 
 const promiseAllTest1 = PromiseAll([1, 2, 3] as const)
 const promiseAllTest2 = PromiseAll([1, 2, Promise.resolve(3)] as const)
-const promiseAllTest3 = PromiseAll([1, 2, Promise.resolve(3)])
+const promiseAllTest3 = PromiseAll([1, 2, Promise.resolve(Promise.resolve('3'))])
 const promiseAllTest4 = PromiseAll<Array<number | Promise<number>>>([1, 2, 3])
 
 type cases = [
   Expect<Equal<typeof promiseAllTest1, Promise<[1, 2, 3]>>>,
   Expect<Equal<typeof promiseAllTest2, Promise<[1, 2, number]>>>,
-  Expect<Equal<typeof promiseAllTest3, Promise<[number, number, number]>>>,
+  Expect<Equal<typeof promiseAllTest3, Promise<[number, number, string]>>>,
   Expect<Equal<typeof promiseAllTest4, Promise<number[]>>>,
 ]
